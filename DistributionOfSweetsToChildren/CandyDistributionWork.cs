2200239ebbca37df215afcf83428526649bbf29c
Дима Ларионов <dima_larionov_000@mail.ru>
2022-12-15 10:58:11 +0300

    Добавьте файлы проекта.

---
+++ DistributionOfSweetsToChildren/CandyDistributionWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionOfSweetsToChildren;
+
+public class CandyDistributionWork
+{
+    public static int DistributionOfSweets(List<int> CandyForChildren)
+    {
+        if (CandyForChildren.Count != 1 && CandyForChildren.Count != 0)
+        {
+            var counter = 0;
+            var theLargestNumber = 0;
+            for (int i = 0; i < CandyForChildren.Count; i++)
+            {
+                if (CandyForChildren[i] > theLargestNumber)
+                {
+                    theLargestNumber = CandyForChildren[i];
+                    continue;
+                }
+            }
+            CandyForChildren.Remove(theLargestNumber);
+            for (int j = 0; j < CandyForChildren.Count;)
+            {
+                if (CandyForChildren[j] < theLargestNumber)
+                {
+                    CandyForChildren[j]++;
+                    counter++;
+                    continue;
+                }
+                else
+                {
+                    j++;
+                    continue;
+                }
+            }
+            return counter;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
---
+++ DistributionOfSweetsToChildren/Program.cs
@@ -0,0 +1,11 @@
+﻿using DistributionOfSweetsToChildren;
+//Задание:
+
+//конфеты([5, 8, 6, 4]) # возврат 9
+//конфеты([1, 2, 4, 6]) # возврат 11
+//конфеты([1, 6]) # возврат 5
+//конфеты([]) # возврат -1
+//конфеты([6]) # возвращают -1 (потому что только один ребенок)
+
+var result = CandyDistributionWork.DistributionOfSweets(new List<int> { 5, 8, 6, 4 });
+Console.WriteLine($"{result} конфет роздано");
